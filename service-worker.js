21026007a1d3ab3eac3ee02ff605a02a63090657
Utkarsh Saxena <137103528+utkarshsaxena1812@users.noreply.github.com>
2025-08-28 00:53:41 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -11,8 +11,17 @@ self.addEventListener("install", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+// push event (basic)
+self.addEventListener("push", event => {
+  const data = event.data ? event.data.text() : "New update in Mess Menu!";
+  event.waitUntil(
+    self.registration.showNotification("Mess Menu", {
+      body: data,
+      icon: "icons/icon-192.png"
     })
   );
 });
